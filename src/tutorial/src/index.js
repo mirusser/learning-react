83ac8c9d9e2273f85edd2447d38151ffa603984a
mirusser <mirusser@gmail.com>
2021-08-21 11:46:46 +0200

    Continuation of the tutorial: css, props , jsx css, jsx javascript

--- src/tutorial/src/index.js
+++ src/tutorial/src/index.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting() {
+//css
+import './css/index.css'
+
+//setup vars
+const author = 'Amelia Hepworth'
+const title = 'I Love You to the Moon and Back'
+const imgSrc =
+  'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg'
+
+const BookList = () => {
   return (
     <>
-      <div>
-        <Person />
-        <Message />
-      </div>
+      <section className='booklist'>
+        <Book job='developer' />
+        <Book title='random title' number={22} />
+      </section>
     </>
   )
 }
 
-const Person = () => <h2>John doe</h2>
-const Message = () => {
-  return <p>this is my message</p>
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src={imgSrc} alt='' />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  )
 }
 
-// const Greeting = () => {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, 'Hello world')
-//   )
-//}
-
-ReactDom.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<BookList />, document.getElementById('root'))
