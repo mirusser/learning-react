d55acc386d5742e76a0e2a8788a2f1d10967ae50
mirusser <mirusser@gmail.com>
2021-08-22 14:06:19 +0200

    Iterating over props

--- src/tutorial/src/index.js
+++ src/tutorial/src/index.js
@@ -5,44 +5,58 @@ import ReactDom from 'react-dom'
 import './css/index.css'
 
 //setup vars
-const firstBook = {
-  author: 'Amelia Hepworth',
-  title: 'I Love You to the Moon and Back',
-  imgSrc:
-    'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
-}
-const secondBook = {
-  author: 'Shannon Olsen',
-  title: 'Our Class is a Family',
-  imgSrc:
-    'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX384_BO1,204,203,200_.jpg',
-}
+const books = [
+  {
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back',
+    imgSrc:
+      'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
+  },
+  {
+    author: 'Shannon Olsen',
+    title: 'Our Class is a Family',
+    imgSrc:
+      'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX384_BO1,204,203,200_.jpg',
+  },
+  {
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    imgSrc:
+      'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._SX339_BO1,204,203,200_.jpg',
+  },
+]
 
 const BookList = () => {
   return (
     <>
       <section className='booklist'>
-        <Book
-          imgSrc={firstBook.imgSrc}
-          title={firstBook.title}
-          author={firstBook.author}
-        />
-        <Book
-          imgSrc={secondBook.imgSrc}
-          title={secondBook.title}
-          author={secondBook.author}
-        />
+        {books.map((book) => {
+          return <Book book={book}></Book>
+        })}
       </section>
     </>
   )
 }
 
-const Book = ({ imgSrc, title, author }) => {
+// const Book = (props) => {
+//   const { imgSrc, title, author } = props.book
+//   return (
+//     <article className='book'>
+//       <img src={imgSrc} alt='' />
+//       <h1>{title}</h1>
+//       <h4>{author.toUpperCase()}</h4>
+//     </article>
+//   )
+// }
+
+const Book = ({ book: { imgSrc, title, author, children } }) => {
   return (
     <article className='book'>
       <img src={imgSrc} alt='' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
+      {children}
     </article>
   )
 }
