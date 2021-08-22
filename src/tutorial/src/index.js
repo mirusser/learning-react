eb910ea2ed3f05c9f1472ec40137188f94648f23
mirusser <mirusser@gmail.com>
2021-08-22 14:12:47 +0200

    Key prop

--- src/tutorial/src/index.js
+++ src/tutorial/src/index.js
@@ -7,18 +7,21 @@ import './css/index.css'
 //setup vars
 const books = [
   {
+    id: 1,
     author: 'Amelia Hepworth',
     title: 'I Love You to the Moon and Back',
     imgSrc:
       'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
   },
   {
+    id: 2,
     author: 'Shannon Olsen',
     title: 'Our Class is a Family',
     imgSrc:
       'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX384_BO1,204,203,200_.jpg',
   },
   {
+    id: 3,
     author: 'James Clear',
     title:
       'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
@@ -32,7 +35,7 @@ const BookList = () => {
     <>
       <section className='booklist'>
         {books.map((book) => {
-          return <Book book={book}></Book>
+          return <Book key={book.id} book={book}></Book>
         })}
       </section>
     </>
