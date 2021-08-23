1dfaca3250becccf1c91c86d43363901791e3ff3
mirusser <mirusser@gmail.com>
2021-08-23 20:57:45 +0200

    use state - object

--- src/tutorial-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  })
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'hello world' })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={() => changeMessage()}>
+        change message
+      </button>
+    </>
+  )
+}
 
-export default UseStateObject;
+export default UseStateObject
