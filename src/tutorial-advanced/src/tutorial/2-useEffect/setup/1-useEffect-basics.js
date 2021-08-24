b31b432f2a0ec1e7b1086a5c832c748e0019e840
mirusser <mirusser@gmail.com>
2021-08-24 10:03:38 +0200

    useEffect - dependency list

--- src/tutorial-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
-// second parameter
+
+//second parameter,
+//not providing second parameter makes that useEffect hook runs every time after render
+//providing [] (empty array) makes that useEffect hook runs only on initial render
+//providing an value to the dependency list (for example [value]) triggers useEffect to run (afeter render) every time the dependency list changes
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0)
   useEffect(() => {
-    console.log('call useEffect')
+    console.log('call useEffect - every time value changed')
     if (value > 0) {
       document.title = `New messages (${value})`
     }
-  })
+  }, [value])
+
+  useEffect(() => {
+    console.log('hello world - only on initial render')
+  }, [])
+
   console.log('render component')
 
   return (
