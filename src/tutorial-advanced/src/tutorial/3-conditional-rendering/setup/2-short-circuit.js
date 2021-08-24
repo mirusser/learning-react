c9f1848c81585acbdbe700e711da60dc1b13c984
mirusser <mirusser@gmail.com>
2021-08-24 16:04:26 +0200

    ternary operator

--- src/tutorial-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,26 +6,31 @@ import React, { useState } from 'react'
 //if one of the operands in short-circuit isn't a boolean type it will be converted to bool for the time of evaluation
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('asdf')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
 
   return (
     <>
       <h2>short circuit</h2>
       {/* <h1>first value: {firstValue}</h1>
       <h1>second value: {secondValue}</h1> */}
-      <h1>text: {text || 'default text'}</h1>
-      <h1>text: {text && 'default text'}</h1>
+      {/* <h1>text: {text || 'default text'}</h1>
+      <h1>text: {text && 'default text'}</h1> */}
 
       {/* same as 'text && 'default text'' */}
-      <h1>
+      {/* <h1>
         {() => {
           if (text) {
             return 'default text'
           }
         }}
-      </h1>
+      </h1> */}
+
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      <h1>{isError && 'Error...'}</h1>
+      {isError ? <p>Error</p> : <p>There is no error</p>}
     </>
   )
 }
