c631d16614d1b66ce0180d545adadae691c415a1
mirusser <mirusser@gmail.com>
2021-08-24 15:04:45 +0200

    short-circuit

--- src/tutorial-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
+//short explanation: short-circuit evaluation return the last evaluated expression (which doesn't need to be bool)
+//if one of the operands in short-circuit isn't a boolean type it will be converted to bool for the time of evaluation
+
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('asdf')
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <>
+      <h2>short circuit</h2>
+      {/* <h1>first value: {firstValue}</h1>
+      <h1>second value: {secondValue}</h1> */}
+      <h1>text: {text || 'default text'}</h1>
+      <h1>text: {text && 'default text'}</h1>
 
-export default ShortCircuit;
+      {/* same as 'text && 'default text'' */}
+      <h1>
+        {() => {
+          if (text) {
+            return 'default text'
+          }
+        }}
+      </h1>
+    </>
+  )
+}
+
+export default ShortCircuit
