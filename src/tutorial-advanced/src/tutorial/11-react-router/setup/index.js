52fa92e33a2e44d07158c59954540a87a3eec39f
mirusser <mirusser@gmail.com>
2021-08-29 11:56:53 +0200

    ReactRouter - basic setup

--- src/tutorial-advanced/src/tutorial/11-react-router/setup/index.js
+++ src/tutorial-advanced/src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from 'react'
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // pages
-import Home from './Home';
-import About from './About';
-import People from './People';
-import Error from './Error';
-import Person from './Person';
+import Home from './Home'
+import About from './About'
+import People from './People'
+import Error from './Error'
+import Person from './Person'
 // navbar
-import Navbar from './Navbar';
-const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
-};
+import Navbar from './Navbar'
 
-export default ReactRouterSetup;
+const ReactRouterSetup = () => {
+  return (
+    <Router>
+      <h2>react router</h2>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/people'>
+        <People />
+      </Route>
+    </Router>
+  )
+}
+
+export default ReactRouterSetup
