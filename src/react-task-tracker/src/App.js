7a571fbf21b1f0bc32f676aaef12a6fa1ce676b5
mirusser <mirusser@gmail.com>
2021-09-03 21:31:59 +0200

    add task - form input state - controlled components

--- src/react-task-tracker/src/App.js
+++ src/react-task-tracker/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
 import Tasks from './Components/Tasks'
+import AddTask from './Components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -19,6 +20,12 @@ function App() {
     },
   ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     console.log(`delete: ${id}`)
     setTasks(tasks.filter((task) => task.id !== id))
@@ -35,6 +42,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
