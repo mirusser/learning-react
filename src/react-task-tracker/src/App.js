562f876a0a9a5f508518f02fa670b923fa0be3c7
mirusser <mirusser@gmail.com>
2021-09-04 09:48:02 +0200

    react-router, useLocation hook,  (footer, about: components)

--- src/react-task-tracker/src/App.js
+++ src/react-task-tracker/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header'
 import Tasks from './Components/Tasks'
 import AddTask from './Components/AddTask'
+import Footer from './Components/Footer'
+import About from './Components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -70,18 +73,35 @@ function App() {
   }
 
   return (
-    <div className='container'>
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        'No tasks'
-      )}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No tasks'
+              )}
+            </>
+          )}
+        />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
--- src/react-task-tracker/src/Components/Header.js
+++ src/react-task-tracker/src/Components/Header.js
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button
-        text={showAdd ? 'Close' : 'Add'}
-        color={showAdd ? 'red' : 'green'}
-        handleClick={() => onAdd()}
-      />
+      {location.pathname === '/' && (
+        <Button
+          text={showAdd ? 'Close' : 'Add'}
+          color={showAdd ? 'red' : 'green'}
+          handleClick={() => onAdd()}
+        />
+      )}
     </header>
   )
 }
