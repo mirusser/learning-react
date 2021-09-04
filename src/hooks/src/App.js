acfd46247a7a16a070caeb1bb5f0bad2ffb09ad3
mirusser <mirusser@gmail.com>
2021-09-04 10:43:35 +0200

    hooks - init

---
+++ src/hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import StateTutorial from './Components/UseState/StateTutorial'
+
+function App() {
+  return (
+    <div className='App'>
+      <StateTutorial />
+    </div>
+  )
+}
+
+export default App
