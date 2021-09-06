44bc20eafda3b24987fadf1ccf2352e431c2ac6f
mirusser <mirusser@gmail.com>
2021-09-06 17:23:42 +0200

    Header - styles

--- src/react-rmdb/src/App.js
+++ src/react-rmdb/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+//styles
 import { GlobalStyle } from './GlobalStyle'
+//components
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       Start here.
       <GlobalStyle />
     </div>
