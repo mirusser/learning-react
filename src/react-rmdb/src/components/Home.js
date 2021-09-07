02ffca2ca83bcdbd1453de599c5b5091b93822be
mirusser <mirusser@gmail.com>
2021-09-07 19:40:33 +0200

    Spinner

--- src/react-rmdb/src/components/Home.js
+++ src/react-rmdb/src/components/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 //components
 import HeroImage from './HeroImage'
 import Grid from './Grid'
 import Thumb from './Thumb'
+import Spinner from './Spinner'
 //hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
@@ -43,6 +44,7 @@ const Home = () => {
           />
         ))}
       </Grid>
+      <Spinner />
     </>
   )
 }
