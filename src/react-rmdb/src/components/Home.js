221767462253070c9e3fd8f30380692ebcdb316d
mirusser <mirusser@gmail.com>
2021-09-07 19:16:29 +0200

    Thumb - component with styling

--- src/react-rmdb/src/components/Home.js
+++ src/react-rmdb/src/components/Home.js
@@ -4,6 +4,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 //components
 import HeroImage from './HeroImage'
 import Grid from './Grid'
+import Thumb from './Thumb'
 //hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
@@ -29,7 +30,17 @@ const Home = () => {
       )}
       <Grid header='Popular Movies'>
         {state.results.map((movie) => (
-          <div key={movie.id}>{movie.title}</div>
+          // <div key={movie.id}>{movie.title}</div>
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage
+            }
+            movieId={movie.id}
+          />
         ))}
       </Grid>
     </>
