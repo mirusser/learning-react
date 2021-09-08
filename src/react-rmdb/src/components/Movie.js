f2102375d3f9ae66e2752d4d4a6ecb5ed8d48e1d
mirusser <mirusser@gmail.com>
2021-09-08 20:25:58 +0200

    MovieInfoBar - component

--- src/react-rmdb/src/components/Movie.js
+++ src/react-rmdb/src/components/Movie.js
@@ -7,6 +7,7 @@ import Grid from './Grid'
 import Spinner from './Spinner'
 import BreadCrumb from './BreadCrumb'
 import MovieInfo from './MovieInfo'
+import MovieInfoBar from './MovieInfoBar'
 //hook
 import { useMovieFetch } from '../hooks/useMovieFetch'
 //image
@@ -23,6 +24,11 @@ const Movie = () => {
     <>
       <BreadCrumb movieTitle={movie.original_title} />
       <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
     </>
   )
 }
