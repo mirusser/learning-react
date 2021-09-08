7c4f6301376d67e7a34eb5e1ba62cb169efb278e
mirusser <mirusser@gmail.com>
2021-09-08 18:16:14 +0200

    routing - react router

--- src/react-rmdb/src/App.js
+++ src/react-rmdb/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
+//routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 //styles
 import { GlobalStyle } from './GlobalStyle'
 //components
 import Header from './components/Header'
 import Home from './components/Home'
+import Movie from './components/Movie'
+import NotFound from './components/NotFound'
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  )
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    <GlobalStyle />
+  </Router>
+)
 
 export default App
