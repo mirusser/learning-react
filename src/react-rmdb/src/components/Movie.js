8f26b8e6d382b9f46433d29270c72083a204a072
mirusser <mirusser@gmail.com>
2021-09-08 18:36:21 +0200

    fetching movie using custom hook - useMovieFetch

--- src/react-rmdb/src/components/Movie.js
+++ src/react-rmdb/src/components/Movie.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+//components
+import Grid from './Grid'
+import Spinner from './Spinner'
+//hook
+import { useMovieFetch } from '../hooks/useMovieFetch'
+//image
+import NoImage from '../images/no_image.jpg'
 
 const Movie = () => {
-  return <div>Movie</div>
+  const { movieId } = useParams()
+  const { state: movie, loading, error } = useMovieFetch(movieId)
+
+  console.log('movie:')
+  console.log(movie)
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  )
 }
 
 export default Movie
