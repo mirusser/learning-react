769c12e1180544bc7a808c1676f3c25a84623972
mirusser <mirusser@gmail.com>
2021-09-08 19:26:24 +0200

    BreadCrumb - component

--- src/react-rmdb/src/components/Movie.js
+++ src/react-rmdb/src/components/Movie.js
@@ -5,6 +5,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 //components
 import Grid from './Grid'
 import Spinner from './Spinner'
+import BreadCrumb from './BreadCrumb'
 //hook
 import { useMovieFetch } from '../hooks/useMovieFetch'
 //image
@@ -14,11 +15,12 @@ const Movie = () => {
   const { movieId } = useParams()
   const { state: movie, loading, error } = useMovieFetch(movieId)
 
-  console.log('movie:')
-  console.log(movie)
+  if (loading) return <Spinner />
+  if (error) return <div>Something went wrong...</div>
+
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
   )
 }
