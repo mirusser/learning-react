7042b97ca9f95502e27c1f2880f9893210f88f4c
mirusser <mirusser@gmail.com>
2021-09-11 18:38:26 +0200

    changed all class components back into function components

--- src/react-rmdb/src/components/Movie.js
+++ src/react-rmdb/src/components/Movie.js
@@ -17,110 +17,112 @@ import NoImage from '../images/no_image.jpg'
 import API from '../API'
 
 //function component:
-// const Movie = () => {
-//   const { movieId } = useParams()
-//   const { state: movie, loading, error } = useMovieFetch(movieId)
+const Movie = () => {
+  const { movieId } = useParams()
+  const { state: movie, loading, error } = useMovieFetch(movieId)
 
-//   if (loading) return <Spinner />
-//   if (error) return <div>Something went wrong...</div>
+  if (loading) return <Spinner />
+  if (error) return <div>Something went wrong...</div>
 
-//   return (
-//     <>
-//       <BreadCrumb movieTitle={movie.original_title} />
-//       <MovieInfo movie={movie} />
-//       <MovieInfoBar
-//         time={movie.runtime}
-//         budget={movie.budget}
-//         revenue={movie.revenue}
-//       />
-//       <Grid header='Actors'>
-//         {movie.actors.map((actor) => (
-//           <Actor
-//             key={actor.creadit_id}
-//             name={actor.name}
-//             character={actor.character}
-//             imageUrl={
-//               actor.profile_path
-//                 ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-//                 : NoImage
-//             }
-//           />
-//         ))}
-//       </Grid>
-//     </>
-//   )
-// }
-
-//class component:
-class Movie extends React.Component {
-  state = {
-    movie: {},
-    loading: true,
-    error: false,
-  }
-
-  fetchMovie = async () => {
-    const { movieId } = this.props.params
-    try {
-      this.setState({ error: false, loading: true })
-      const movie = await API.fetchMovie(movieId)
-      const credits = await API.fetchCredits(movieId)
-      //get directors only
-      const directors = credits.crew.filter(
-        (member) => member.job === 'Director'
-      )
-
-      this.setState({
-        movie: {
-          ...movie,
-          actors: credits.cast,
-          directors,
-        },
-        loading: false,
-      })
-    } catch (error) {
-      this.setState({ error: true, loading: false })
-    }
-  }
-
-  componentDidMount() {
-    this.fetchMovie()
-  }
-
-  render() {
-    const { movie, loading, error } = this.state
-    if (loading) return <Spinner />
-
-    if (error) return <div>Something went wrong...</div>
-
-    return (
-      <>
-        <BreadCrumb movieTitle={movie.original_title} />
-        <MovieInfo movie={movie} />
-        <MovieInfoBar
-          time={movie.runtime}
-          budget={movie.budget}
-          revenue={movie.revenue}
-        />
-        <Grid header='Actors'>
-          {movie.actors.map((actor) => (
-            <Actor
-              key={actor.credit_id}
-              name={actor.name}
-              character={actor.character}
-              imageUrl={
-                actor.profile_path
-                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-                  : NoImage
-              }
-            />
-          ))}
-        </Grid>
-      </>
-    )
-  }
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header='Actors'>
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.creadit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
+    </>
+  )
 }
 
-const MovieWithParams = (props) => <Movie {...props} params={useParams()} />
+export default Movie
 
-export default MovieWithParams
+//class component:
+// class Movie extends React.Component {
+//   state = {
+//     movie: {},
+//     loading: true,
+//     error: false,
+//   }
+
+//   fetchMovie = async () => {
+//     const { movieId } = this.props.params
+//     try {
+//       this.setState({ error: false, loading: true })
+//       const movie = await API.fetchMovie(movieId)
+//       const credits = await API.fetchCredits(movieId)
+//       //get directors only
+//       const directors = credits.crew.filter(
+//         (member) => member.job === 'Director'
+//       )
+
+//       this.setState({
+//         movie: {
+//           ...movie,
+//           actors: credits.cast,
+//           directors,
+//         },
+//         loading: false,
+//       })
+//     } catch (error) {
+//       this.setState({ error: true, loading: false })
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.fetchMovie()
+//   }
+
+//   render() {
+//     const { movie, loading, error } = this.state
+//     if (loading) return <Spinner />
+
+//     if (error) return <div>Something went wrong...</div>
+
+//     return (
+//       <>
+//         <BreadCrumb movieTitle={movie.original_title} />
+//         <MovieInfo movie={movie} />
+//         <MovieInfoBar
+//           time={movie.runtime}
+//           budget={movie.budget}
+//           revenue={movie.revenue}
+//         />
+//         <Grid header='Actors'>
+//           {movie.actors.map((actor) => (
+//             <Actor
+//               key={actor.credit_id}
+//               name={actor.name}
+//               character={actor.character}
+//               imageUrl={
+//                 actor.profile_path
+//                   ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+//                   : NoImage
+//               }
+//             />
+//           ))}
+//         </Grid>
+//       </>
+//     )
+//   }
+// }
+
+// const MovieWithParams = (props) => <Movie {...props} params={useParams()} />
+
+// export default MovieWithParams
--- src/react-rmdb/src/components/SearchBar/index.js
+++ src/react-rmdb/src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 //image
 import searchIcon from '../../images/search-icon.svg'
@@ -6,73 +6,73 @@ import searchIcon from '../../images/search-icon.svg'
 import { Wrapper, Content } from './SearchBar.styles'
 
 //function component:
-// const SearchBar = ({ setSearchTerm }) => {
-//   const [state, setState] = useState('')
-//   const initial = useRef(true)
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('')
+  const initial = useRef(true)
 
-//   useEffect(() => {
-//     if (initial.current) {
-//       initial.current = false
-//       return
-//     }
-//     const timer = setTimeout(() => {
-//       setSearchTerm(state)
-//     }, 500)
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false
+      return
+    }
+    const timer = setTimeout(() => {
+      setSearchTerm(state)
+    }, 500)
 
-//     return () => clearTimeout(timer)
-//   }, [setSearchTerm, state])
+    return () => clearTimeout(timer)
+  }, [setSearchTerm, state])
 
-//   return (
-//     <Wrapper>
-//       <Content>
-//         <img src={searchIcon} alt='search-icon' />
-//         <input
-//           type='text'
-//           placeholder='Search movie'
-//           onChange={(e) => setState(e.currentTarget.value)}
-//           value={state}
-//         />
-//       </Content>
-//     </Wrapper>
-//   )
-// }
+  return (
+    <Wrapper>
+      <Content>
+        <img src={searchIcon} alt='search-icon' />
+        <input
+          type='text'
+          placeholder='Search movie'
+          onChange={(e) => setState(e.currentTarget.value)}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  )
+}
 
 //class component:
-class SearchBar extends Component {
-  state = { value: '' }
-  timeout = null
+// class SearchBar extends Component {
+//   state = { value: '' }
+//   timeout = null
 
-  componentDidUpdate(_prevProps, prevState) {
-    if (this.state.value !== prevState.value) {
-      const { setSearchTerm } = this.props
+//   componentDidUpdate(_prevProps, prevState) {
+//     if (this.state.value !== prevState.value) {
+//       const { setSearchTerm } = this.props
 
-      clearTimeout(this.timeout)
+//       clearTimeout(this.timeout)
 
-      this.timeout = setTimeout(() => {
-        const { value } = this.state
-        setSearchTerm(value)
-      }, 500)
-    }
-  }
+//       this.timeout = setTimeout(() => {
+//         const { value } = this.state
+//         setSearchTerm(value)
+//       }, 500)
+//     }
+//   }
 
-  render() {
-    const { value } = this.state
+//   render() {
+//     const { value } = this.state
 
-    return (
-      <Wrapper>
-        <Content>
-          <img src={searchIcon} alt='search-icon' />
-          <input
-            type='text'
-            placeholder='Search movie'
-            onChange={(e) => this.setState({ value: e.currentTarget.value })}
-            value={value}
-          />
-        </Content>
-      </Wrapper>
-    )
-  }
-}
+//     return (
+//       <Wrapper>
+//         <Content>
+//           <img src={searchIcon} alt='search-icon' />
+//           <input
+//             type='text'
+//             placeholder='Search movie'
+//             onChange={(e) => this.setState({ value: e.currentTarget.value })}
+//             value={value}
+//           />
+//         </Content>
+//       </Wrapper>
+//     )
+//   }
+// }
 
 SearchBar.propTypes = {
   callback: PropTypes.func,
