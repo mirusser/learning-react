bac2e07a095c936a3ab59d77cc3e9292fa5d9133
mirusser <mirusser@gmail.com>
2021-09-11 18:21:13 +0200

    changed Home component to be class component instead of function compoents

--- src/react-rmdb/src/components/Home.js
+++ src/react-rmdb/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 //components
@@ -13,44 +13,138 @@ import { useHomeFetch } from '../hooks/useHomeFetch'
 
 //image
 import NoImage from '../images/no_image.jpg'
+//api
+import API from '../API'
 
-const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } =
-    useHomeFetch()
+//function component:
+// const Home = () => {
+//   const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } =
+//     useHomeFetch()
 
-  if (error) return <div>Something went wrong ...</div>
+//   if (error) return <div>Something went wrong ...</div>
 
-  return (
-    <>
-      {!searchTerm && state.results[0] ? (
-        <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-          title={state.results[0].original_title}
-          text={state.results[0].overview}
-        />
-      ) : null}
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <Grid header={searchTerm ? 'Search result' : 'Popular Movies'}>
-        {state.results.map((movie) => (
-          // <div key={movie.id}>{movie.title}</div>
-          <Thumb
-            key={movie.id}
-            clickable
-            image={
-              movie.poster_path
-                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-                : NoImage
-            }
-            movieId={movie.id}
+//   return (
+//     <>
+//       {!searchTerm && state.results[0] ? (
+//         <HeroImage
+//           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+//           title={state.results[0].original_title}
+//           text={state.results[0].overview}
+//         />
+//       ) : null}
+//       <SearchBar setSearchTerm={setSearchTerm} />
+//       <Grid header={searchTerm ? 'Search result' : 'Popular Movies'}>
+//         {state.results.map((movie) => (
+//           // <div key={movie.id}>{movie.title}</div>
+//           <Thumb
+//             key={movie.id}
+//             clickable
+//             image={
+//               movie.poster_path
+//                 ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+//                 : NoImage
+//             }
+//             movieId={movie.id}
+//           />
+//         ))}
+//       </Grid>
+//       {loading && <Spinner />}
+//       {state.page < state.total_pages && !loading && (
+//         <Button text='Load more' callback={() => setIsLoadingMore(true)} />
+//       )}
+//     </>
+//   )
+// }
+
+//class component:
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+}
+
+class Home extends Component {
+  state = {
+    movies: initialState,
+    searchTerm: '',
+    isLoadingMore: false,
+    loading: false,
+    error: false,
+  }
+
+  fetchMovies = async (page, searchTerm = '') => {
+    try {
+      this.setState({ error: false, loading: true })
+
+      const movies = await API.fetchMovies(searchTerm, page)
+
+      this.setState((prev) => ({
+        ...prev,
+        movies: {
+          ...movies,
+          results:
+            page > 1
+              ? [...prev.movies.results, ...movies.results]
+              : [...movies.results],
+        },
+        loading: false,
+      }))
+    } catch (error) {
+      this.setState({ error: true, loading: false })
+    }
+  }
+
+  handleSearch = (searchTerm) => {
+    this.setState({ movies: initialState, searchTerm }, () => {
+      this.fetchMovies(1, this.state.searchTerm)
+    })
+  }
+
+  handleLoadMore = () => {
+    this.fetchMovies(this.state.movies.page + 1, this.state.searchTerm)
+  }
+
+  componentDidMount() {
+    this.fetchMovies(1)
+  }
+
+  render() {
+    const { searchTerm, movies, loading, error } = this.state
+    if (error) return <div>Something went wrong ...</div>
+
+    return (
+      <>
+        {!searchTerm && movies.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies.results[0].backdrop_path}`}
+            title={movies.results[0].original_title}
+            text={movies.results[0].overview}
           />
-        ))}
-      </Grid>
-      {loading && <Spinner />}
-      {state.page < state.total_pages && !loading && (
-        <Button text='Load more' callback={() => setIsLoadingMore(true)} />
-      )}
-    </>
-  )
+        ) : null}
+        <SearchBar setSearchTerm={this.handleSearch} />
+        <Grid header={searchTerm ? 'Search result' : 'Popular Movies'}>
+          {movies.results.map((movie) => (
+            // <div key={movie.id}>{movie.title}</div>
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                  : NoImage
+              }
+              movieId={movie.id}
+            />
+          ))}
+        </Grid>
+        {loading && <Spinner />}
+        {movies.page < movies.total_pages && !loading && (
+          <Button text='Load more' callback={() => this.handleLoadMore(true)} />
+        )}
+      </>
+    )
+  }
 }
 
 export default Home
