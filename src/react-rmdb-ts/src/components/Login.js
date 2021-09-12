e590209bb8ed1443a6ce663992a5fdf0fbe44fcf
mirusser <mirusser@gmail.com>
2021-09-12 13:45:43 +0200

    Login component

--- src/react-rmdb-ts/src/components/Login.js
+++ src/react-rmdb-ts/src/components/Login.js
@@ -9,25 +9,53 @@ import { Wrapper } from './Login.styles'
 import { Context } from '../context'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
 
-  const handleSubmit = () => {}
-  
-  const handleInput = (e) => {}
+  const [_user, setUser] = useContext(Context)
+  const navigate = useNavigate()
+
+  const handleSubmit = async () => {
+    setError(false)
+    try {
+      const requestToken = await API.getRequestToken()
+      const sessionId = await API.authenticate(requestToken, username, password)
+      console.log(sessionId)
+      setUser({ sessionId: sessionId.session_id, username })
+
+      navigate('/') //after login navigate(go to) home page
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  const handleInput = (e) => {
+    const name = e.currentTarget.name
+    const value = e.currentTarget.value
+
+    if (name === 'username') setUsername(value)
+    else if (name === 'password') setPassword(value)
+  }
 
   return (
     <Wrapper>
+      {error && <div className='error'>There was an error</div>}
       <label>Username:</label>
       <input
         type='text'
-        value='state value'
+        value={username}
         name='username'
         onChange={handleInput}
+        placeholder='Enter name here...'
       />
+      <label>Password:</label>
       <input
         type='password'
-        value='state value'
+        value={password}
         name='password'
         onChange={handleInput}
+        placeholder='Enter password here...'
       />
       <Button text='Login' callback={handleSubmit} />
     </Wrapper>
